b2e0a7067f2af0ccfa6e90dcb680ab76663aaa4d
Osmar Coronel <oac2@illinois.edu>
2018-10-20 02:10:43 -0400

    made the chatbot better

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,56 @@
 import random
 import json
-
-chat_history = {}
+import sys
+import csv
 
 def main():
-	with open('data.json') as f:
+	chat_history = {}
+	with open('data.json', 'r') as f:
 		chat_history = json.load(f)
-
-def end():
-	with open('data.json', 'w') as f:
-		json.dump(chat_history, f)
 		
+		shouldLearn = raw_input("Learn or Chat: ")
+		if ( shouldLearn == "learn"):
+			learnChat(chat_history)
+		else:
+			runChat(chat_history)
+		
+		#learnMovies(chat_history)
+		closeFile(chat_history)
+		
+def learnMovies(chat_history):
+	with open('movie_lines 2.tsv', 'r') as f:
+		tsvin = csv.reader(f, delimiter='\t')
+		convoNum = None
+		previous = None
+		for row in tsvin:
+			if 2 >= len(row):
+				continue
+			if convoNum is None:
+				convoNum = row[2]
+			if row[2] == convoNum and previous is not None:
+				learn(chat_history, previous.strip().lower(), row[4].strip().lower())
+				previous = row[4]
+			else:
+				previous = row[4]
 
-def response(text):
+def runChat(chat_history):
+	line = raw_input("User: ")
+	while line != "quit":
+		print "Ricottas:", response(chat_history, line.strip().lower())
+		line = raw_input("User: ")
+
+def learnChat(chat_history):
+	for line in sys.stdin:
+        	text, response = line.split("|")
+		text = text.strip().lower()
+		response = response.strip().lower()
+        	learn(chat_history, text, response)
+
+def closeFile(chat_history):
+	with open('data.json', 'w') as f:	
+		json.dump(chat_history, f)
+
+def response(chat_history, text):
 	if text in chat_history:
 		nodes = chat_history[text]
 		total_responses = sum([i[1] for i in nodes])
@@ -24,10 +62,13 @@ def response(text):
 				return i[0]
 			current += prob
 	else:
-		chat_history[text] = []
+		print "Sorry I do not know that! :("
+		response = raw_input("How should I respond: ")
+		chat_history[text] = [(response, 1)]
+		return response
 
 
-def learn(text, response):
+def learn(chat_history, text, response):
 	if text in chat_history:
 		nodes = chat_history[text]
 		for i in range(len(nodes)):
@@ -41,5 +82,3 @@ def learn(text, response):
 		chat_history[text] = [(response, 1.0)]
 
 main()
-
-end()
